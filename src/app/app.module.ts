7f84cf5ca1f617e4c6e56d8315a8042196627f89
Raju <rajuprasad7919@gmail.com>
2024-08-09 15:35:15 +0530

    project arhitecture created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartComponent } from './pages/admin/cart/cart.component';
+import { CustmerComponent } from './pages/admin/custmer/custmer.component';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { OrderComponent } from './pages/admin/order/order.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { CategoryProductsComponent } from './pages/website/category-products/category-products.component';
+import { CheckoutComponent } from './pages/website/checkout/checkout.component';
+import { CustomerCartComponent } from './pages/website/customer-cart/customer-cart.component';
+import { CustomerOrdersComponent } from './pages/website/customer-orders/customer-orders.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CustmerComponent,
+    CategoriesComponent,
+    LayoutComponent,
+    LoginComponent,
+    OrderComponent,
+    ProductsComponent,
+    CategoryProductsComponent,
+    CheckoutComponent,
+    CustomerCartComponent,
+    CustomerOrdersComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/admin/custmer/custmer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custmer',
+  templateUrl: './custmer.component.html',
+  styleUrls: ['./custmer.component.scss']
+})
+export class CustmerComponent {
+
+}
